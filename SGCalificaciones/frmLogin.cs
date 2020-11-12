e536947e917c4324143999fa30f2bc3c1ceb12d2
GabrielaChoque <abejaatareada629@gmail.com>
2020-11-12 00:05:21 -0400

    Creación del formulario de Login

--- SGCalificaciones/frmLogin.cs
+++ SGCalificaciones/frmLogin.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SGCalificaciones
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
         }
