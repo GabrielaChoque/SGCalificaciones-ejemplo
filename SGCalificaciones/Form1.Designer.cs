a9f19dfcb293c8294813a5b5830985ffe06ef75e
GabrielaChoque <abejaatareada629@gmail.com>
2020-11-11 23:55:47 -0400

    Conexión con la base de datos y adición del FontAwesome.sharp

--- SGCalificaciones/Form1.Designer.cs
+++ SGCalificaciones/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 37);
+            this.button1.Location = new System.Drawing.Point(245, 33);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 279);
+            this.ClientSize = new System.Drawing.Size(580, 317);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
