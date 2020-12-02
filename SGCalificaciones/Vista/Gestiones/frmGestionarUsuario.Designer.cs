98d16b646a0820a06726e4c52a05c0eb66edab85
GabrielaChoque <abejaatareada629@gmail.com>
2020-12-02 10:01:29 -0400

    frmCuenta para cada usuario

---
+++ SGCalificaciones/Vista/Gestiones/frmGestionarUsuario.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace SGCalificaciones.Vista.Gestiones
+{
+    partial class frmGestionarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label ap_maternoLabel;
+            System.Windows.Forms.Label ap_paternoLabel;
+            System.Windows.Forms.Label celularLabel;
+            System.Windows.Forms.Label contraseniaLabel;
+            System.Windows.Forms.Label nombreLabel;
+            System.Windows.Forms.Label nro_carnetLabel;
+            System.Windows.Forms.Label tipo_plantelLabel;
+            System.Windows.Forms.Label usuarioLabel;
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.plantel_EducativoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ap_maternoTextBox = new System.Windows.Forms.TextBox();
+            this.ap_paternoTextBox = new System.Windows.Forms.TextBox();
+            this.celularTextBox = new System.Windows.Forms.TextBox();
+            this.contraseniaTextBox = new System.Windows.Forms.TextBox();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.nro_carnetTextBox = new System.Windows.Forms.TextBox();
+            this.usuarioTextBox = new System.Windows.Forms.TextBox();
+            this.tipo_plantelTextBox = new System.Windows.Forms.TextBox();
+            ap_maternoLabel = new System.Windows.Forms.Label();
+            ap_paternoLabel = new System.Windows.Forms.Label();
+            celularLabel = new System.Windows.Forms.Label();
+            contraseniaLabel = new System.Windows.Forms.Label();
+            nombreLabel = new System.Windows.Forms.Label();
+            nro_carnetLabel = new System.Windows.Forms.Label();
+            tipo_plantelLabel = new System.Windows.Forms.Label();
+            usuarioLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plantel_EducativoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ap_maternoLabel
+            // 
+            ap_maternoLabel.AutoSize = true;
+            ap_maternoLabel.Location = new System.Drawing.Point(348, 317);
+            ap_maternoLabel.Name = "ap_maternoLabel";
+            ap_maternoLabel.Size = new System.Drawing.Size(119, 13);
+            ap_maternoLabel.TabIndex = 20;
+            ap_maternoLabel.Text = "APELLIDO MATERNO:";
+            // 
+            // ap_paternoLabel
+            // 
+            ap_paternoLabel.AutoSize = true;
+            ap_paternoLabel.Location = new System.Drawing.Point(348, 291);
+            ap_paternoLabel.Name = "ap_paternoLabel";
+            ap_paternoLabel.Size = new System.Drawing.Size(117, 13);
+            ap_paternoLabel.TabIndex = 22;
+            ap_paternoLabel.Text = "APELLIDO PATERNO:";
+            // 
+            // celularLabel
+            // 
+            celularLabel.AutoSize = true;
+            celularLabel.Location = new System.Drawing.Point(348, 344);
+            celularLabel.Name = "celularLabel";
+            celularLabel.Size = new System.Drawing.Size(59, 13);
+            celularLabel.TabIndex = 24;
+            celularLabel.Text = "CELULAR:";
+            // 
+            // contraseniaLabel
+            // 
+            contraseniaLabel.AutoSize = true;
+            contraseniaLabel.Location = new System.Drawing.Point(348, 422);
+            contraseniaLabel.Name = "contraseniaLabel";
+            contraseniaLabel.Size = new System.Drawing.Size(84, 13);
+            contraseniaLabel.TabIndex = 26;
+            contraseniaLabel.Text = "CONTRASEÑA:";
+            // 
+            // nombreLabel
+            // 
+            nombreLabel.AutoSize = true;
+            nombreLabel.Location = new System.Drawing.Point(348, 266);
+            nombreLabel.Name = "nombreLabel";
+            nombreLabel.Size = new System.Drawing.Size(57, 13);
+            nombreLabel.TabIndex = 28;
+            nombreLabel.Text = "NOMBRE:";
+            // 
+            // nro_carnetLabel
+            // 
+            nro_carnetLabel.AutoSize = true;
+            nro_carnetLabel.Location = new System.Drawing.Point(348, 240);
+            nro_carnetLabel.Name = "nro_carnetLabel";
+            nro_carnetLabel.Size = new System.Drawing.Size(123, 13);
+            nro_carnetLabel.TabIndex = 30;
+            nro_carnetLabel.Text = "NUMERO DE CARNET:";
+            // 
+            // tipo_plantelLabel
+            // 
+            tipo_plantelLabel.AutoSize = true;
+            tipo_plantelLabel.Location = new System.Drawing.Point(348, 370);
+            tipo_plantelLabel.Name = "tipo_plantelLabel";
+            tipo_plantelLabel.Size = new System.Drawing.Size(48, 13);
+            tipo_plantelLabel.TabIndex = 32;
+            tipo_plantelLabel.Text = "CARGO:";
+            // 
+            // usuarioLabel
+            // 
+            usuarioLabel.AutoSize = true;
+            usuarioLabel.Location = new System.Drawing.Point(348, 396);
+            usuarioLabel.Name = "usuarioLabel";
+            usuarioLabel.Size = new System.Drawing.Size(59, 13);
+            usuarioLabel.TabIndex = 34;
+            usuarioLabel.Text = "USUARIO:";
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.Color.White;
+            this.iconPictureBox2.ForeColor = System.Drawing.Color.Crimson;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Times;
+            this.iconPictureBox2.IconColor = System.Drawing.Color.Crimson;
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox2.Location = new System.Drawing.Point(994, 23);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox2.TabIndex = 17;
+            this.iconPictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Regatto", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(399, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 47);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "MI CUENTA";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.White;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.UserTie;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 92;
+            this.iconPictureBox1.Location = new System.Drawing.Point(464, 123);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(92, 97);
+            this.iconPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.iconPictureBox1.TabIndex = 19;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // plantel_EducativoBindingSource
+            // 
+            this.plantel_EducativoBindingSource.DataSource = typeof(SGCalificaciones.Data.Plantel_Educativo);
+            // 
+            // ap_maternoTextBox
+            // 
+            this.ap_maternoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "ap_materno", true));
+            this.ap_maternoTextBox.Enabled = false;
+            this.ap_maternoTextBox.Location = new System.Drawing.Point(493, 314);
+            this.ap_maternoTextBox.Name = "ap_maternoTextBox";
+            this.ap_maternoTextBox.Size = new System.Drawing.Size(184, 20);
+            this.ap_maternoTextBox.TabIndex = 21;
+            // 
+            // ap_paternoTextBox
+            // 
+            this.ap_paternoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "ap_paterno", true));
+            this.ap_paternoTextBox.Enabled = false;
+            this.ap_paternoTextBox.Location = new System.Drawing.Point(493, 288);
+            this.ap_paternoTextBox.Name = "ap_paternoTextBox";
+            this.ap_paternoTextBox.Size = new System.Drawing.Size(184, 20);
+            this.ap_paternoTextBox.TabIndex = 23;
+            // 
+            // celularTextBox
+            // 
+            this.celularTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "celular", true));
+            this.celularTextBox.Enabled = false;
+            this.celularTextBox.Location = new System.Drawing.Point(493, 341);
+            this.celularTextBox.Name = "celularTextBox";
+            this.celularTextBox.Size = new System.Drawing.Size(184, 20);
+            this.celularTextBox.TabIndex = 25;
+            // 
+            // contraseniaTextBox
+            // 
+            this.contraseniaTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "contrasenia", true));
+            this.contraseniaTextBox.Enabled = false;
+            this.contraseniaTextBox.Location = new System.Drawing.Point(493, 419);
+            this.contraseniaTextBox.Name = "contraseniaTextBox";
+            this.contraseniaTextBox.Size = new System.Drawing.Size(184, 20);
+            this.contraseniaTextBox.TabIndex = 27;
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "nombre", true));
+            this.nombreTextBox.Enabled = false;
+            this.nombreTextBox.Location = new System.Drawing.Point(493, 263);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(184, 20);
+            this.nombreTextBox.TabIndex = 29;
+            // 
+            // nro_carnetTextBox
+            // 
+            this.nro_carnetTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "nro_carnet", true));
+            this.nro_carnetTextBox.Enabled = false;
+            this.nro_carnetTextBox.Location = new System.Drawing.Point(493, 237);
+            this.nro_carnetTextBox.Name = "nro_carnetTextBox";
+            this.nro_carnetTextBox.Size = new System.Drawing.Size(184, 20);
+            this.nro_carnetTextBox.TabIndex = 31;
+            // 
+            // usuarioTextBox
+            // 
+            this.usuarioTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "usuario", true));
+            this.usuarioTextBox.Enabled = false;
+            this.usuarioTextBox.Location = new System.Drawing.Point(493, 393);
+            this.usuarioTextBox.Name = "usuarioTextBox";
+            this.usuarioTextBox.Size = new System.Drawing.Size(184, 20);
+            this.usuarioTextBox.TabIndex = 35;
+            // 
+            // tipo_plantelTextBox
+            // 
+            this.tipo_plantelTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.plantel_EducativoBindingSource, "tipo_plantel", true));
+            this.tipo_plantelTextBox.Enabled = false;
+            this.tipo_plantelTextBox.Location = new System.Drawing.Point(493, 367);
+            this.tipo_plantelTextBox.Name = "tipo_plantelTextBox";
+            this.tipo_plantelTextBox.Size = new System.Drawing.Size(184, 20);
+            this.tipo_plantelTextBox.TabIndex = 36;
+            // 
+            // frmGestionarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1078, 669);
+            this.Controls.Add(this.tipo_plantelTextBox);
+            this.Controls.Add(ap_maternoLabel);
+            this.Controls.Add(this.ap_maternoTextBox);
+            this.Controls.Add(ap_paternoLabel);
+            this.Controls.Add(this.ap_paternoTextBox);
+            this.Controls.Add(celularLabel);
+            this.Controls.Add(this.celularTextBox);
+            this.Controls.Add(contraseniaLabel);
+            this.Controls.Add(this.contraseniaTextBox);
+            this.Controls.Add(nombreLabel);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(nro_carnetLabel);
+            this.Controls.Add(this.nro_carnetTextBox);
+            this.Controls.Add(tipo_plantelLabel);
+            this.Controls.Add(usuarioLabel);
+            this.Controls.Add(this.usuarioTextBox);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iconPictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmGestionarUsuario";
+            this.Text = "frmGestionarUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plantel_EducativoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.BindingSource plantel_EducativoBindingSource;
+        private System.Windows.Forms.TextBox ap_maternoTextBox;
+        private System.Windows.Forms.TextBox ap_paternoTextBox;
+        private System.Windows.Forms.TextBox celularTextBox;
+        private System.Windows.Forms.TextBox contraseniaTextBox;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.TextBox nro_carnetTextBox;
+        private System.Windows.Forms.TextBox usuarioTextBox;
+        private System.Windows.Forms.TextBox tipo_plantelTextBox;
+    }
+}
---
+++ SGCalificaciones/Vista/Gestiones/frmGestionarUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGCalificaciones.Vista.Gestiones
+{
+    public partial class frmGestionarUsuario : Form
+    {
+        public frmGestionarUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
